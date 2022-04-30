3437f0b680204a3dbed06deb0b42deca88aade87
Juan <juanandres15@gmail.com>
2022-04-30 02:37:50 +0100

    projects challenges page

---
+++ script.js
@@ -0,0 +1,30 @@
+const projects = [
+    'single-price-grid-component-master',
+    'challenge-2',
+    'challenge-3',
+    'challenge-4'
+];
+
+const list=document.getElementById('list');
+console.log(list)
+projects.forEach((project, i) => {
+    const listItem = document.createElement('li');
+    const link = document.createElement('a');
+    link.href = `/${project}/index.html`;
+    link.innerText = `${i + 1}.${formatProjectName(project)}`;
+
+    const img = document.createElement('img');
+    img.src = `${project}/design/desktop-design.jpg`;
+
+    link.prepend(img);
+    listItem.appendChild(link);
+    list.appendChild(listItem);
+});
+
+function formatProjectName(name) {
+    return name
+    .split('-')
+    .map(word => word[0].toUpperCase() + word.slice(1)
+    )
+    .join(' ');
+}
